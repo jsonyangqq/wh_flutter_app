c3d687f908a52d7af1c98126a521a79892a40c7f
yifan <1554667256@qq.com>
2019-12-06 17:59:18 +0800

    提交工单列表展示

--- lib/config/Config.dart
+++ lib/config/Config.dart
@@ -1,5 +1,5 @@
 
 class Config{
-    static String domain="http://k7j285.natappfree.cc/wx_tenthousand_no";
+    static String domain="http://5wxmen.natappfree.cc/wx_tenthousand_no";
 //  static String domain="http://www.whgcs.cn/wx_tenthousand_no";
 }
